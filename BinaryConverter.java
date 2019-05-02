316a4e4cc5c115b83ddcea1f06b0525d0981e814
Brynn Haley <brynn.haley@live.com>
2019-05-01 22:35:17 -0300

    Initial commit of program. Tested and working.

---
+++ BinaryConverter.java
@@ -0,0 +1,36 @@
+public class BinaryConverter{
+
+	private int input;
+	private int temp;
+	private String result = "";
+	
+	
+	public BinaryConverter(int input){
+		this.input = input;
+	}
+
+	public void convert(int input){
+	
+		while(input !=0){
+		temp = input % 2;
+		input = input/2;
+		result += Integer.toString(temp);
+		}
+		
+	}
+	
+	public void printResult(String result){
+		for(int i = result.length()-1; i >= 0; i--){
+			System.out.print(result.charAt(i));
+		}
+	}
+	
+
+
+	public static void main(String args[]){
+		BinaryConverter c = new BinaryConverter(23);
+		c.convert(12);
+		c.printResult(c.result);
+	}
+}
+
