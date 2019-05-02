a754187447b254b1fefb4823e365de522dd3cfad
Brynn Haley <brynn.haley@live.com>
2019-05-01 22:47:28 -0300

    Minor formatting fixes.

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -4,19 +4,16 @@ public class BinaryConverter{
 	private int temp;
 	private String result = "";
 	
-	
 	public BinaryConverter(int input){
 		this.input = input;
 	}
 
 	public void convert(int input){
-	
 		while(input !=0){
 		temp = input % 2;
 		input = input/2;
 		result += Integer.toString(temp);
 		}
-		
 	}
 	
 	public void printResult(String result){
@@ -25,11 +22,9 @@ public class BinaryConverter{
 		}
 	}
 	
-
-
 	public static void main(String args[]){
 		BinaryConverter c = new BinaryConverter(23);
-		c.convert(12);
+		c.convert(446545);
 		c.printResult(c.result);
 	}
 }
